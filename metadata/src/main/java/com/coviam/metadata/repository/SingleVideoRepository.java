2989157bdefddd3bfcff9dcc640c978ab28237cf
Shivam2498 <shivamchoudhary825@gmail.com>
2019-08-01 20:24:31 +0530

    Changed service in episode service

---
+++ metadata/src/main/java/com/coviam/metadata/repository/SingleVideoRepository.java
@@ -0,0 +1,8 @@
+package com.coviam.metadata.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SingleVideoRepository extends CrudRepository {
+}
---
+++ metadata/src/main/java/com/coviam/metadata/services/SingleVideoServices.java
@@ -0,0 +1,7 @@
+package com.coviam.metadata.services;
+
+import com.coviam.metadata.dto.SingleVideoDto;
+
+public interface SingleVideoServices {
+    boolean addSingleVideo(SingleVideoDto singleVideoDto);
+}
