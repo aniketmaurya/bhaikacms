2c69eb8207cd2349c0ef339f6d82ad6068d46003
divya <1605273@kiit.ac.in>
2019-08-01 12:50:16 +0530

    first commit

---
+++ useradmin/src/main/java/com/cmssystem/useradmin/dto/UserAdminDetailsDto.java
@@ -0,0 +1,41 @@
+package com.cmssystem.useradmin.dto;
+
+public class UserAdminDetailsDto {
+
+    private String firstName;
+    private String lastName;
+    private String password;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "UserAdminDetailsDto{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
