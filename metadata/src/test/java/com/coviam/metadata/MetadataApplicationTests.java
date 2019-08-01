f0e5ed2dda4f02499d7ec754255e7165b1c77fe5
nupurpandey <y>
2019-08-01 13:47:02 +0530

    metadata

---
+++ metadata/src/test/java/com/coviam/metadata/MetadataApplicationTests.java
@@ -0,0 +1,16 @@
+package com.coviam.metadata;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MetadataApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
