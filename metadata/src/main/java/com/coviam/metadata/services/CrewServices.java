6784fd18235752cec970edf24b8f7685b062a6e7
Shivam2498 <shivamchoudhary825@gmail.com>
2019-08-06 23:05:08 +0530

    Added addSeasonInBulk

--- metadata/src/main/java/com/coviam/metadata/services/CrewServices.java
+++ metadata/src/main/java/com/coviam/metadata/services/CrewServices.java
@@ -1,15 +1,11 @@
 package com.coviam.metadata.services;
 
 import com.coviam.metadata.entity.Crew;
-import org.springframework.data.domain.Page;
 
 import java.util.List;
 
 public interface CrewServices {
 
     Boolean addCrew(List<Crew> crewList);
-
-    Page<Crew> getAllCrew(Integer pageNumber, Integer pageSize);
-
     Boolean deleteCrew(String role, String name);
 }
--- metadata/src/main/java/com/coviam/metadata/services/impl/CrewServiceImpl.java
+++ metadata/src/main/java/com/coviam/metadata/services/impl/CrewServiceImpl.java
@@ -4,8 +4,6 @@ import com.coviam.metadata.entity.Crew;
 import com.coviam.metadata.repository.CrewRepository;
 import com.coviam.metadata.services.CrewServices;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,11 +18,6 @@ public class CrewServiceImpl implements CrewServices {
     public Boolean addCrew(List<Crew> crewList) {
         crewRepository.saveAll(crewList);
         return Boolean.TRUE;
-    }
-
-    @Override
-    public Page<Crew> getAllCrew(Integer pageNumber, Integer pageSize) {
-        return crewRepository.findAll(PageRequest.of(pageNumber, pageSize));
     }
 
     @Override
